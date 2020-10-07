618d9011f5186bf0644ca83e1a7d4bf3fb073664
drewfidel <62400308+drewfidel@users.noreply.github.com>
2020-10-07 16:06:28 -0700

    slot machine styles css test

--- Section 3/SlotMachine_Exercise/Machine.js
+++ Section 3/SlotMachine_Exercise/Machine.js
@@ -1,11 +1,16 @@
 class Machine extends React.Component {
-  render() {
-    const {s1,s2,s3} = this.props;
-    const winner = (s1 === s2) && (s2===s3);
-    return(
-      <div>
-        <p>{s1} {s2} {s3}</p>
-        <p>{winner ? 'winner': 'loser'}</p>
+  render(){
+    const {s1, s2, s3} = this.props;
+    const winnings = (s1 === s2) && (s2 === s3);
+    const color = { fontSize: "50px", backgroundColor: 'purple' };
+    
+    return (
+      <div className="Machine">
+        <p style={color}>
+          {s1} {s2} {s3}
+        </p>
+        <p className={winnings ? 'win' : 'lose'}>
+          {winnings ? "You win!" : "You lose!"}</p>
       </div>
     );
   }
